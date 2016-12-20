804f1687fa637afd17c9f18d624123214d27adf7
Francisco Suero <suerofrancisco@yahoo.com>
2016-12-20 13:56:24 -0500

    fixed big search bug

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,9 @@
 class SearchController < ApplicationController
 
   def index
+    @title = params[:query][:title]
+    @artist = params[:query][:artist]
+    @location = params[:query][:location_description]
     title = "%25#{params[:query][:title]}%25"
     artist = "%25#{params[:query][:artist]}%25"
     location = "%25#{params[:query][:location_description]}%25"
